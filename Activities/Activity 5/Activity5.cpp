76688bebe2ef801a02a23f3615cc64b812f9c18f
Nes370 <rie_fu_fan@yahoo.com>
2020-09-10 11:17:15 -0700

    Removed redundant castings

--- Activities/Activity 5/Activity5.cpp
+++ Activities/Activity 5/Activity5.cpp
@@ -14,8 +14,10 @@ using namespace std;
 
 int main() {
 
+    double monthlyRate, loanAmount, monthlyPayment, totalAmountPaid;
+    int numberOfMonths;
+
     // Prompt for annual interest rate, Rate * 12
-    double monthlyRate, loanAmount, numberOfMonths, monthlyPayment, totalAmountPaid;
     cout << "Enter an annual interest rate: ";
     cin >> monthlyRate;
     monthlyRate /= 1200.0;
@@ -29,8 +31,8 @@ int main() {
     cin >> numberOfMonths;
 
     // Payment = (Rate * pow(1 + Rate, N)) / (pow(1 + Rate, N) - 1) * L
-    monthlyPayment = (monthlyRate * pow(1 + monthlyRate, static_cast<double>(numberOfMonths)))
-         / (pow(1 + monthlyRate, static_cast<double>(numberOfMonths)) - 1) * loanAmount;
+    monthlyPayment = (monthlyRate * pow(1 + monthlyRate, numberOfMonths))
+         / (pow(1 + monthlyRate, numberOfMonths) - 1) * loanAmount;
 
     // Discard trailing decimal digits
     monthlyPayment = static_cast<int>(monthlyPayment * 100) / 100.0;
@@ -43,7 +45,7 @@ int main() {
     cout << fixed  << showpoint << setprecision(2);
     cout << setw(24) << "Loan Amount:" << "  $ " << setw(8) << loanAmount << '\n'
         << setw(24) << "Monthly Interest Rate:" << "    " << setw(8) << monthlyRate * 100 << "%\n"
-        << setw(24) << "Number of Payments:" << "    " << setw(5) << static_cast<int>(numberOfMonths) << '\n'
+        << setw(24) << "Number of Payments:" << "    " << setw(5) << numberOfMonths << '\n'
         << setw(24) << "Monthly Payment:" << "  $ " << setw(8) << monthlyPayment << '\n'
         << setw(24) << "Amount Paid Back:" << "  $ " << setw(8) << totalAmountPaid << '\n'
         << setw(24) << "Interest Paid:" <<  "  $ " << setw(8) << totalAmountPaid - loanAmount << endl;
